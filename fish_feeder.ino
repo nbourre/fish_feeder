0d94f6b13c662424dc1b8c5cad00de6f0ee83764
Nick B <nbourrereg@hotmail.com>
2019-07-14 21:04:04 -0400

    Added a blinking pattern for status

--- fish_feeder.ino
+++ fish_feeder.ino
@@ -13,14 +13,34 @@
 #define ONE_DAY ONE_HOUR * 24
 #define ONE_REV -4000 // Number of steps required for a full rotation
 
+#define PUSH_REV 0.2 * ONE_REV
+#define RETRACT_REV 0.05 * ONE_REV
+
 enum MachineState {
+  BOOTING,
   WAITING,
   PUSHING,
   RETRACTING,
   TESTING
 };
 
-MachineState currentState = WAITING;
+
+
+MachineState currentState = BOOTING;
+
+enum BlinkingState {
+  HOURS,
+  MINUTES,
+  HEARTBEAT,
+  END_MESSAGE,
+  PAUSE,
+  NONE
+  
+};
+
+BlinkingState blinkingState = HEARTBEAT;
+int blink_count = 0;
+
 float overRevfactor = 0.25; // Factor used to push a bit more food an retract a bit more
 
 // Initialize with pin sequence IN1-IN3-IN2-IN4 for using the AccelStepper with 28BYJ-48
@@ -32,13 +52,25 @@ long pt = 0; // previous time
 long dt = 0; // delta time
 
 long waiting_acc = 0;
-long waiting_delay = 10 * ONE_SECOND;
+long waiting_delay = ONE_DAY;
 
 // heart beat
 long hb_acc = 0; 
 long hb_delay = ONE_SECOND;
 int hb_led = 13;
 int hb_state = HIGH;
+
+long pause_acc = 0;
+long pause_delay = ONE_SECOND;
+
+long msg_end_acc = 0;
+long msg_end_delay = ONE_SECOND;
+long msg_end_blink_delay = ONE_SECOND >> 3;
+
+long msg_time_delay = ONE_SECOND / 3;
+
+long booting_acc = 0;
+long booting_delay = ONE_SECOND * 5;
 
 bool stateStarted = false;
 
@@ -50,9 +82,9 @@ void setup() {
   stepper1.setAcceleration(50);
   stepper1.setSpeed(300);
 
-  Serial.println ("SETUP : Purging 4 revolutions (blocking)");
-  stepper1.moveTo(4 * ONE_REV);
-  stepper1.runToPosition();
+//  Serial.println ("SETUP : Purging 4 revolutions (blocking)");
+//  stepper1.moveTo(5 * ONE_REV);
+//  stepper1.runToPosition();
 
   pinMode(hb_led, OUTPUT);
 
@@ -65,6 +97,9 @@ void loop() {
   pt = ct;
 
   switch (currentState) {
+    case BOOTING:
+      bootingTask();
+      break;
     case WAITING:
       waitTask();
       break;
@@ -83,11 +118,16 @@ void loop() {
   stepper1.run();
 }
 
+
+/// Just a blinking task
+/// Change hb_delay in other task
 void heartbeatTask() {
   hb_acc += dt;
 
   if (hb_acc >= hb_delay) {
     hb_acc = 0;
+
+    blink_count++;
 
     hb_state = hb_state == HIGH ? LOW : HIGH;
 
@@ -96,21 +136,96 @@ void heartbeatTask() {
   
 }
 
+int waiting_left = 0;
+int waiting_left_last = 0;
+int waiting_left_hours = 0;
+int waiting_left_tens = 0; // tens of minutes
+
+int start_blink_count = 0;
+
 void waitTask() {
   waiting_acc += dt;
   
   if (!stateStarted) {
     Serial.println ("Doing waiting task");
     waiting_acc= 0;
+    blink_count = 0;
     
     stepper1.disableOutputs();
 
-    hb_delay = ONE_SECOND;
+    hb_delay = msg_time_delay;
+
     stateStarted = true;
   } else {
+
+    // Blinking time left in hours and tens minutes
+    waiting_left = (int)((waiting_delay - waiting_acc) / 60000); // Get time left in minutes
+    waiting_left_tens = (waiting_left % 60) / 10;
     
+    if (waiting_left_tens != waiting_left_last) {
+      Serial.print ("Blinking delay : ");
+      Serial.println (hb_delay);
+      
+      waiting_left_hours = waiting_left / 60;
+
+      hb_delay = msg_time_delay;
+
+      start_blink_count = blink_count;
+      blinkingState = HOURS;
+
+      Serial.print("Time left --> ");
+      Serial.print(waiting_left_hours);
+      Serial.print(" hours ");
+      Serial.print(waiting_left_tens);
+      Serial.println(" tens minute");
+    }
+
+    int blink_delta = (blink_count - start_blink_count) / 2;
+
+    switch (blinkingState) {
+      case HOURS:
+        if (blink_delta >= waiting_left_hours){
+          Serial.println ("Blinking hours left");
+          hb_delay = ONE_SECOND * 2;
+          blinkingState = PAUSE;
+          start_blink_count = blink_count;
+        }        
+        break;
+      case PAUSE:
+        pause_acc += dt;
+        if (pause_acc >= pause_delay) {
+          pause_acc = 0;
+          blinkingState = MINUTES;
+          hb_delay = msg_time_delay;
+        }
+        break;
+      case MINUTES:
+        if (blink_delta >= waiting_left_tens){
+          Serial.println ("Blinking tens of minutes left");
+          hb_delay = msg_end_blink_delay;
+          blinkingState = END_MESSAGE;
+          start_blink_count = blink_count;
+        }              
+        break;
+      case END_MESSAGE:
+        msg_end_acc += dt;
+        if (msg_end_acc >= msg_end_delay){
+          msg_end_acc = 0;
+          
+          Serial.println ("Blinking end_message");
+          hb_delay = msg_time_delay;
+          blinkingState = HOURS;
+          start_blink_count = blink_count;
+        }              
+        break;
+      default:
+        break;
+    }
+    
+    waiting_left_last = waiting_left_tens;
   }
-  
+
+  // Done waiting going to next state
   if (waiting_acc >= waiting_delay) {
     Serial.println ("Waiting done");
     waiting_acc = 0;
@@ -125,9 +240,9 @@ void pushTask() {
     Serial.println ("Doing pushing task");
     stepper1.enableOutputs();
     stepper1.setCurrentPosition(0);
-    stepper1.moveTo((1 + 2 * overRevfactor) * ONE_REV);
+    stepper1.moveTo(PUSH_REV);
 
-    hb_delay = 0.2 * ONE_SECOND;
+    hb_delay = 0.1 * ONE_SECOND;
 
     stateStarted = true;
   }
@@ -145,7 +260,7 @@ void retractTask(){
     Serial.println ("Doing retracting task");
 
     stepper1.setCurrentPosition(0);
-    stepper1.moveTo(-overRevfactor * ONE_REV);
+    stepper1.moveTo(RETRACT_REV);
 
     hb_delay = 0.5 * ONE_SECOND;
     stateStarted = true;
@@ -157,6 +272,31 @@ void retractTask(){
     stateStarted = false;
     currentState = WAITING;
   }  
+}
+
+void bootingTask() {
+  if (!stateStarted) {
+    Serial.println ("Booting for 5 seconds");
+
+    booting_acc = 0;
+    blink_count = 0;
+    
+    stepper1.disableOutputs();
+
+    hb_delay = msg_time_delay;
+
+    stateStarted = true;
+  } else {
+    booting_acc += dt;
+
+    if (booting_acc >= booting_delay) {
+      booting_acc = 0;
+
+      stateStarted = false;
+      currentState = PUSHING;
+    }
+  }
+  
 }
 
 void testTask() {
